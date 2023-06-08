7d1f1492e82f8192946dc7bf7c1e3be5ee98a7a9
Khalid <khaliddakar001@gmail.com>
2023-06-08 11:46:22 +0300

    fixed the todo form

--- to-do-list/src/components/Todoform.js
+++ to-do-list/src/components/Todoform.js
@@ -1,28 +1,3 @@
-// import React, {useState} from 'react'
-
-// const Todoform = ({addTodo}) => {
-
-// const [value, setValue] = useState('');
-
-// const handleSubmit = e =>{
-//   e.preventDefault();
-
-//   addTodo(value)
-
-//   setValue("")
-// }
-
-// return (
-//     <frorm className='TodoForm' onSubmit={handleSubmit} >
-//     <input type="text" className='todo-input' value={value} placeholder='What is the task today?' onChange={(e)=> setValue(e.target.value) } />
-//     <button type='submit' className='todo-btn'>ADD TASK</button>
-//    </frorm>
-// )
-
-// }
-
-// export default Todoform
-
 import React, {useState} from 'react'
 
 export const TodoForm = ({addTodo}) => {
